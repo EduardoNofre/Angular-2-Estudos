6ba4ecfe99f3953cfe05941ae29920423000cb5c
eduardo.sa <eduardo.sa@gps-pamcary.com.br>
2024-11-24 13:49:03 -0300

    mais exemplos

--- ciclo-de-vida-componente/src/app/app.component.ts
+++ ciclo-de-vida-componente/src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'ciclo-de-vida-componente';
+  valor: number = 5;
+  destruirCicloEvent: boolean = false;
+
+  public mudarValor() {
+    this.valor++;
+  }
+
+  public destruirCiclo() {
+    this.destruirCicloEvent = true;
+  }
 }
--- ciclo-de-vida-componente/src/app/componente-filha-ciclo-de-vida/componente-filha-ciclo-de-vida.component.ts
+++ ciclo-de-vida-componente/src/app/componente-filha-ciclo-de-vida/componente-filha-ciclo-de-vida.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-componente-filha-ciclo-de-vida',
   templateUrl: './componente-filha-ciclo-de-vida.component.html',
-  styleUrl: './componente-filha-ciclo-de-vida.component.css'
+  styleUrl: './componente-filha-ciclo-de-vida.component.css',
 })
-export class ComponenteFilhaCicloDeVidaComponent {
+export class ComponenteFilhaCicloDeVidaComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    OnDestroy,
+    OnDestroy,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewChecked,
+    AfterViewInit,
+    AfterViewChecked
+{
 
+  @Input() valorInicialInput:number = 10;
+
+
+  public constructor() {
+    this.log('constructor');
+  }
+
+  public ngOnInit() {
+    this.log('ngOnInit');
+  }
+
+  public ngOnChanges() {
+    this.log('ngOnChanges');
+  }
+
+  public ngDoCheck() {
+    this.log('ngDoCheck');
+  }
+
+  public ngOnDestroy() {
+    this.log('ngOnDestroy');
+  }
+
+  public ngAfterContentChecked() {
+    this.log('ngAfterContentChecked');
+  }
+
+  public ngAfterContentInit() {
+    this.log('ngAfterContentInit');
+  }
+
+  public ngAfterViewChecked() {
+    this.log('ngAfterViewChecked');
+  }
+
+  public ngAfterViewInit() {
+    this.log('ngAfterViewInit');
+  }
+
+  private log(hook: String) {
+    console.log(hook);
+  }
 }
